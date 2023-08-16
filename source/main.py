be0853195e373971681f441917733abac01afecf
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-08-16 16:14:04 -0300

    more cleanup

--- source/main.py
+++ source/main.py
@@ -1,34 +1,8 @@
-# Version 3, ahora es personal
-
-# Controles:
-# WASD o flechas -> movimiento
-# h              -> pista
-# b              -> deshacer para el último movimiento hecho
-# r              -> reiniciar el nivel
-# Esc            -> salir
-
-# Se pueden cambiar los controles modificando el archivo "teclas.txt"
-
-# Cosas extras de debug:
-# tipear ascii     -> cambia la representación del juego a caracteres ascii
-# tipear off       -> vuelve a la representación original
-# tipear debug     -> da acceso a dos teclas extra
-# n                -> ir al siguiente nivel
-# p                -> volver al anterior nivel 
-
-# niveles sacados de
-# http://www.sneezingtiger.com/sokoban/levels/microbanText.html
-
-# documentación de gamelib
-# https://dessaya.github.io/python-gamelib/
-
-
 import soko
 import gamelib
 from modulo_cola_pila import Pila
 import sys
 import os
-import stat
 import subprocess
 
 NORTE = (0 ,-1)
--- source/readme.md
+++ source/readme.md
@@ -7,8 +7,6 @@ Controles:
   r              -> reiniciar el nivel
   Esc            -> salir
 
-Se pueden cambiar los controles modificando el archivo "teclas.txt"
-
 Cosas extras de debug:
   tipear ascii     -> cambia la representación del juego a caracteres ascii
   tipear off       -> vuelve a la representación original
@@ -19,5 +17,5 @@ Cosas extras de debug:
 niveles sacados de
   http://www.sneezingtiger.com/sokoban/levels/microbanText.html
 
-documentación de gamelib
-  https://dessaya.github.io/python-gamelib/
+Página de github de gamelib
+  https://github.com/dessaya/python-gamelib
